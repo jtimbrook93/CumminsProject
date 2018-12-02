5fc4b4ecc313a14d34162f1fb4f820ac93d20d8e
jtimbrook93 <jtimbroo@iu.edu>
2018-12-02 13:38:01 -0500

    fix script

--- public/JS/authentication.app.js
+++ public/JS/authentication.app.js
@@ -5,7 +5,7 @@ var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
 if ( username == "Formget" && password == "formget#123"){
 alert ("Login successfully");
-window.location = "success.html"; // Redirecting to other page.
+window.location = "myProducts.html"; // Redirecting to other page.
 return false;
 }
 else{
