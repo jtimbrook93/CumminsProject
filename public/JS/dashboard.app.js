2cd92b12e2e037f09baa0fb709142a7893532c57
jtimbrook93 <jtimbroo@iu.edu>
2018-12-03 22:07:51 -0500

    dont know

--- public/JS/dashboard.app.js
+++ public/JS/dashboard.app.js
@@ -54,7 +54,7 @@ var dashboardApp = new Vue ({
         console.log(dashboardApp.dataValue);
         this.buildChart();
       },
-      
+
       productChange(){
         console.log(dashboardApp.productValue);
         this.buildChart();
@@ -256,7 +256,7 @@ var dashboardApp = new Vue ({
 
         const url = new URL(window.location.href);
         const cid = url.searchParams.get('customerId') || 0;
-        const pn = url.searchParams.get('productName') || 0;
+        const pn = url.searchParams.get('productName') || "6.7L Cummins Turbo Diesel for Pickups (2016)";
 
         this.formatDate();
         this.getData(cid, pn);
