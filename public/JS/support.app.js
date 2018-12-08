b191f66156efc09f6cb9d6c446cfb9d18ed6474e
jtimbrook93 <jtimbroo@iu.edu>
2018-12-07 19:31:44 -0500

    chatbox

--- public/JS/support.app.js
+++ public/JS/support.app.js
@@ -10,15 +10,7 @@ computed: {
   },
 
   methods: {
-      function(){
-        $("#addClass").click(function () {
-        $('#qnimate').addClass('popup-box-on');
-          });
-
-          $("#removeClass").click(function () {
-        $('#qnimate').removeClass('popup-box-on');
-          });
-}
+      
   },
 
   created () {
