b068ac38f908e04b77f9a380f4603b8b7e267afe
jtimbrook93 <jtimbroo@iu.edu>
2018-12-09 17:41:38 -0500

    testomg

--- public/JS/dashboard.app.js
+++ public/JS/dashboard.app.js
@@ -31,7 +31,8 @@ var dashboardApp = new Vue ({
 
     dataArr: [],
 
-      dataArr2: []
+    dataArr2: [],
+    dataSerial: []
 
 
 
@@ -43,16 +44,16 @@ var dashboardApp = new Vue ({
 
   methods: {
 
-   // TODO: build displayCustomerReport()
+    // TODO: build displayCustomerReport()
 
-        // myFilter: function (productName, , dataArr) {
-        //   for(var i = 0; i < idx; i++) {
-        //     if(arr[i].name === val.name) {
-        //       return false;
-        //     }
-        //   }
-        //   return true;
-        // },
+    // myFilter: function (productName, , dataArr) {
+    //   for(var i = 0; i < idx; i++) {
+    //     if(arr[i].name === val.name) {
+    //       return false;
+    //     }
+    //   }
+    //   return true;
+    // },
 
     getProductName(cid){
       fetch('api/myproducts.php?customerId='+cid)
@@ -64,263 +65,261 @@ var dashboardApp = new Vue ({
           console.log(err);
         });
 
-    },
-
-    getSerialNumber(cid, pn){
-      fetch('api/serialNumber.php?customerId='+cid +'&productName='+pn)
-      .then( response => response.json() )  // "a => expression" is shorthand function declaration
-      .then( json => {
-        dashboardApp.dataArr = json;  })
-        .catch( err => {
-          console.log('METRIC LIST FETCH ERROR:');
-          console.log(err);
-        });
-
-    },
-
-
-    getData(cid, pn, sn) {
-      fetch('api/dashboard.php?customerId='+cid +'&productName='+pn +'&serialNumber='+sn)
-      .then( response => response.json() )  // "a => expression" is shorthand function declaration
-      .then( json => {
-        dashboardApp.dataArr2 = json;  } )
-        .catch( err => {
-          console.log('METRIC LIST FETCH ERROR:');
-          console.log(err);
-        });
-
-        this.formatDate();
       },
 
-      valueChange(){
-        console.log(dashboardApp.dataValue);
-        this.buildChart();
-      },
-
-      productChange(){
-        console.log(dashboardApp.productValue);
-        this.getSerialNumber(dashboardApp.tempCid, dashboardApp.productValue);
-        //this.getData(dashboardApp.tempCid, dashboardApp.productValue, dashboardApp.serialNumberValue);
-        //this.buildChart();
-      },
-
-      serialNumberChange(){
-        console.log(dashboardApp.serialNumberValue);
-        this.getData(dashboardApp.tempCid, dashboardApp.productValue, dashboardApp.serialNumberValue);
-        this.buildChart();
-      },
-
-
-      formatDate(){
-        this.dataArr2.forEach(
-          function(entry) {
-            entry.dateCollected = Date.parse(entry.dateCollected); // Convert to ms since Jan 1, 1970 UTC
-
+      getSerialNumber(cid, pn){
+        fetch('api/serialNumber.php?customerId='+cid +'&productName='+pn)
+        .then( response => response.json() )  // "a => expression" is shorthand function declaration
+        .then( json => {
+          dashboardApp.dataSerial = json;  })
+          .catch( err => {
+            console.log('METRIC LIST FETCH ERROR:');
+            console.log(err);
           });
-        },
-
-        pretty_date: function (d) {
-          return moment(d).format('l')
 
         },
-        buildChart(){
-          Highcharts.chart('grafiek_bench_rendement', {
 
-            title: {
-              text: 'Engine Diognostics Report'
+
+        getData(cid, pn, sn) {
+          fetch('api/dashboard.php?customerId='+cid +'&productName='+pn +'&serialNumber='+sn)
+          .then( response => response.json() )  // "a => expression" is shorthand function declaration
+          .then( json => {
+            dashboardApp.dataArr2 = json;  } )
+            .catch( err => {
+              console.log('METRIC LIST FETCH ERROR:');
+              console.log(err);
+            });
+
+            this.formatDate();
+          },
+
+          valueChange(){
+            console.log(dashboardApp.dataValue);
+          },
+
+          productChange(){
+            console.log(dashboardApp.productValue);
+            this.getSerialNumber(dashboardApp.tempCid, dashboardApp.productValue);
+            //this.getData(dashboardApp.tempCid, dashboardApp.productValue, dashboardApp.serialNumberValue);
+            //this.buildChart();
+          },
+
+          serialNumberChange(){
+            console.log(dashboardApp.serialNumberValue);
+            this.getData(dashboardApp.tempCid, dashboardApp.productValue, dashboardApp.serialNumberValue);
+            this.buildChart();
+          },
+
+
+          formatDate(){
+            this.dataArr2.forEach(
+              function(entry) {
+                entry.dateCollected = Date.parse(entry.dateCollected); // Convert to ms since Jan 1, 1970 UTC
+
+              });
             },
 
-            subtitle: {
-              text: 'Lifetime statistics'
+            pretty_date: function (d) {
+              return moment(d).format('l')
+
             },
-            xAxis: {
-              type: 'datetime'
-            },
-            yAxis: {
-              title: {
-                text: this.getName()
-              }
-            },
-            legend: {
-              layout: 'vertical',
-              align: 'right',
-              verticalAlign: 'middle'
-            },
-            plotOptions: {
-              area: {
-                fillColor: {
-                  linearGradient: {
-                    x1: 0,
-                    y1: 0,
-                    x2: 0,
-                    y2: 1
-                  },
-                  stops: [
-                    [0, Highcharts.getOptions().colors[0]],
-                    [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
-                  ]
+            buildChart(){
+              Highcharts.chart('grafiek_bench_rendement', {
+
+                title: {
+                  text: 'Engine Diognostics Report'
                 },
-                marker: {
-                  radius: 2
-                },
 
-                lineWidth: 1,
-                states: {
-                  hover: {
-                    lineWidth: 1
+                subtitle: {
+                  text: 'Lifetime statistics'
+                },
+                xAxis: {
+                  type: 'datetime'
+                },
+                yAxis: {
+                  title: {
+                    text: this.getName()
                   }
                 },
-
-                threshold: null
-              }
-            },
-            series:
-              this.getSeries(),
-
-            responsive: {
-              rules: [{
-                condition: {
-                  maxWidth: 500
+                legend: {
+                  layout: 'vertical',
+                  align: 'right',
+                  verticalAlign: 'middle'
                 },
-                chartOptions: {
-                  legend: {
-                    layout: 'horizontal',
-                    align: 'center',
-                    verticalAlign: 'bottom'
+                plotOptions: {
+                  area: {
+                    fillColor: {
+                      linearGradient: {
+                        x1: 0,
+                        y1: 0,
+                        x2: 0,
+                        y2: 1
+                      },
+                      stops: [
+                        [0, Highcharts.getOptions().colors[0]],
+                        [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                      ]
+                    },
+                    marker: {
+                      radius: 2
+                    },
+
+                    lineWidth: 1,
+                    states: {
+                      hover: {
+                        lineWidth: 1
+                      }
+                    },
+
+                    threshold: null
                   }
+                },
+                series:
+                this.getSeries(),
+
+                responsive: {
+                  rules: [{
+                    condition: {
+                      maxWidth: 500
+                    },
+                    chartOptions: {
+                      legend: {
+                        layout: 'horizontal',
+                        align: 'center',
+                        verticalAlign: 'bottom'
+                      }
+                    }
+                  }]
                 }
-              }]
+              });
+            },
+
+            getSeries(){
+              var series = [];
+              if(this.dataValue=='airMassFlowRate')
+              {
+                series = [{name: 'airMassFlowRate', data: this.dataArr2.map( item => [Date.parse(item.dateCollected), item.airMassFlowRate])}]
+              }
+              else if(this.dataValue=='fuelMassFlowRate')
+              {
+                series = [{name: 'fuelMassFlowRate', data: this.dataArr2.map( item => [Date.parse(item.dateCollected), item.fuelMassFlowRate])}]
+              }
+              else if(this.dataValue == "drag")
+              {
+                series = [{name: 'drag', data: this.dataArr2.map( item => [Date.parse(item.dateCollected), item.drag])}]
+              }
+              else if(this.dataValue=='thrust')
+              {
+                series = [{name: 'thrust', data: this.dataArr2.map( item => [Date.parse(item.dateCollected), item.thrust])}]
+              }
+              else if(this.dataValue == "fuelBurned")
+              {
+                series = [{name: 'fuelBurned', data: this.dataArr2.map( item => [Date.parse(item.dateCollected), item.fuelBurned])}]
+              }
+              else if(this.dataValue=='fuelEfficiency')
+              {
+                series = [{name: 'fuelEfficiency', data: this.dataArr2.map( item => [Date.parse(item.dateCollected), item.fuelEfficiency])}]
+              }
+              else if(this.dataValue == "noxLevels")
+              {
+                series = [{name: 'noxLevels', data: this.dataArr2.map( item => [Date.parse(item.dateCollected), item.noxLevels])}]
+              }
+              else if(this.dataValue=='momentumChangeAMF')
+              {
+                series = [{name: 'momentumChangeAMF', data: this.dataArr2.map( item => [Date.parse(item.dateCollected), item.momentumChangeAMF])}]
+              }
+              else if(this.dataValue == "momentumChangeFMF")
+              {
+                series = [{name: 'momentumChangeFMF', data: this.dataArr2.map( item => [Date.parse(item.dateCollected), item.momentumChangeFMF])}]
+              }
+              else if(this.dataValue=='energyBalance')
+              {
+                series = [{name: 'energyBalance', data: this.dataArr2.map( item => [Date.parse(item.dateCollected), item.energyBalance])}]
+              }
+              else if(this.dataValue == "propulsiveEfficiency")
+              {
+                series = [{name: 'propulsiveEfficiency', data: this.dataArr2.map( item => [Date.parse(item.dateCollected), item.propulsiveEfficiency])}]
+              }
+              else if(this.dataValue=='thermalEfficiency')
+              {
+                series = [{name: 'thermalEfficiency', data: this.dataArr2.map( item => [Date.parse(item.dateCollected), item.thermalEfficiency])}]
+              }
+              return series;
+            },
+
+            getName(){
+              var name = [];
+              if(this.dataValue=='airMassFlowRate')
+              {
+                name = 'Air Mass Flow Rate'
+              }
+              else if(this.dataValue=='fuelMassFlowRate')
+              {
+                name = 'Fuel Mass Flow Rate'
+              }
+              else if(this.dataValue == "drag")
+              {
+                name = 'Drag'
+              }
+              else if(this.dataValue=='thrust')
+              {
+                name = 'Thrust'
+              }
+              else if(this.dataValue == "fuelBurned")
+              {
+                name = 'Fuel Burned'
+              }
+              else if(this.dataValue=='fuelEfficiency')
+              {
+                name = 'Fuel Efficiency'
+              }
+              else if(this.dataValue == "noxLevels")
+              {
+                name = 'NOx Levels'
+              }
+              else if(this.dataValue=='momentumChangeAMF')
+              {
+                name = 'Momentum Change AMF'
+              }
+              else if(this.dataValue == "momentumChangeFMF")
+              {
+                name = 'Momentum Change FMF'
+              }
+              else if(this.dataValue=='energyBalance')
+              {
+                name = 'Energy Balance'
+              }
+              else if(this.dataValue == "propulsiveEfficiency")
+              {
+                name = 'Propultive Efficiency'
+              }
+              else if(this.dataValue=='thermalEfficiency')
+              {
+                name = 'Thermal Efficiency'
+              }
+              return name;
             }
-      });
-      },
 
-        getSeries(){
-          var series = [];
-          if(this.dataValue=='airMassFlowRate')
-          {
-            series = [{name: 'airMassFlowRate', data: this.dataArr2.map( item => [Date.parse(item.dateCollected), item.airMassFlowRate])}]
-          }
-          else if(this.dataValue=='fuelMassFlowRate')
-          {
-            series = [{name: 'fuelMassFlowRate', data: this.dataArr2.map( item => [Date.parse(item.dateCollected), item.fuelMassFlowRate])}]
-          }
-          else if(this.dataValue == "drag")
-          {
-            series = [{name: 'drag', data: this.dataArr2.map( item => [Date.parse(item.dateCollected), item.drag])}]
-          }
-          else if(this.dataValue=='thrust')
-          {
-          series = [{name: 'thrust', data: this.dataArr2.map( item => [Date.parse(item.dateCollected), item.thrust])}]
-          }
-          else if(this.dataValue == "fuelBurned")
-         {
-          series = [{name: 'fuelBurned', data: this.dataArr2.map( item => [Date.parse(item.dateCollected), item.fuelBurned])}]
-          }
-          else if(this.dataValue=='fuelEfficiency')
-          {
-            series = [{name: 'fuelEfficiency', data: this.dataArr2.map( item => [Date.parse(item.dateCollected), item.fuelEfficiency])}]
-          }
-          else if(this.dataValue == "noxLevels")
-          {
-            series = [{name: 'noxLevels', data: this.dataArr2.map( item => [Date.parse(item.dateCollected), item.noxLevels])}]
-          }
-          else if(this.dataValue=='momentumChangeAMF')
-          {
-          series = [{name: 'momentumChangeAMF', data: this.dataArr2.map( item => [Date.parse(item.dateCollected), item.momentumChangeAMF])}]
-          }
-          else if(this.dataValue == "momentumChangeFMF")
-         {
-          series = [{name: 'momentumChangeFMF', data: this.dataArr2.map( item => [Date.parse(item.dateCollected), item.momentumChangeFMF])}]
-          }
-          else if(this.dataValue=='energyBalance')
-          {
-          series = [{name: 'energyBalance', data: this.dataArr2.map( item => [Date.parse(item.dateCollected), item.energyBalance])}]
-          }
-          else if(this.dataValue == "propulsiveEfficiency")
-         {
-          series = [{name: 'propulsiveEfficiency', data: this.dataArr2.map( item => [Date.parse(item.dateCollected), item.propulsiveEfficiency])}]
-          }
-          else if(this.dataValue=='thermalEfficiency')
-          {
-            series = [{name: 'thermalEfficiency', data: this.dataArr2.map( item => [Date.parse(item.dateCollected), item.thermalEfficiency])}]
-          }
-          return series;
-      },
+          },
 
-      getName(){
-        var name = [];
-        if(this.dataValue=='airMassFlowRate')
-        {
-          name = 'Air Mass Flow Rate'
-        }
-        else if(this.dataValue=='fuelMassFlowRate')
-        {
-          name = 'Fuel Mass Flow Rate'
-        }
-        else if(this.dataValue == "drag")
-        {
-          name = 'Drag'
-        }
-        else if(this.dataValue=='thrust')
-        {
-          name = 'Thrust'
-        }
-        else if(this.dataValue == "fuelBurned")
-        {
-          name = 'Fuel Burned'
-        }
-        else if(this.dataValue=='fuelEfficiency')
-        {
-          name = 'Fuel Efficiency'
-        }
-        else if(this.dataValue == "noxLevels")
-        {
-          name = 'NOx Levels'
-        }
-        else if(this.dataValue=='momentumChangeAMF')
-        {
-          name = 'Momentum Change AMF'
-        }
-        else if(this.dataValue == "momentumChangeFMF")
-        {
-          name = 'Momentum Change FMF'
-        }
-        else if(this.dataValue=='energyBalance')
-        {
-          name = 'Energy Balance'
-        }
-        else if(this.dataValue == "propulsiveEfficiency")
-        {
-          name = 'Propultive Efficiency'
-        }
-        else if(this.dataValue=='thermalEfficiency')
-        {
-          name = 'Thermal Efficiency'
-        }
-        return name;
-    }
+          created () {
 
-},
+            const url = new URL(window.location.href);
+            const cid = url.searchParams.get('customerId') || 0;
+            const pn = url.searchParams.get('productName') || "";
+            const sn = url.searchParams.get('serialNumber') || "";
 
-      created () {
+            this.tempCid = cid;
+            this.productValue = pn;
+            this.serialNumberValue = sn;
 
-        const url = new URL(window.location.href);
-        const cid = url.searchParams.get('customerId') || 0;
-        const pn = url.searchParams.get('productName') || "";
-        const sn = url.searchParams.get('serialNumber') || "";
+            // this.productCategories ();
+            this.getProductName(cid);
+            //this.getSerialNumber(cid, pn);
+            this.formatDate();
+            //this.getData(cid, pn);
+            this.getSeries();
+            this.getName();
 
-        this.tempCid = cid;
-        this.productValue = pn;
-        this.serialNumberValue = sn;
-
-        // this.productCategories ();
-        this.getProductName(cid);
-        //this.getSerialNumber(cid, pn);
-        this.buildChart();
-        this.formatDate();
-        //this.getData(cid, pn);
-        this.getSeries();
-        this.getName();
-
-      }
-    });
+          }
+        });
