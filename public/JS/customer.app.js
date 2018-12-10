f15906f674d6e8f13dca32a5d06dd2ad081a8023
jtimbrook93 <jtimbroo@iu.edu>
2018-12-10 18:09:23 -0500

    id

--- public/JS/customer.app.js
+++ public/JS/customer.app.js
@@ -19,7 +19,7 @@ data: {
     methods: {
 
       displayCustomerReport(){
-        fetch('api/customer.php?Id='+ document.getElementById('customerId').value)
+        fetch('api/customer.php?Id='+id)
         .then( response => response.json() )  // "a => expression" is shorthand function declaration
         .then( json => {
           console.log(json);
@@ -35,10 +35,12 @@ data: {
 
       created () {
 
+
+
         const url = new URL(window.location.href);
         const id = url.searchParams.get('Id') || 0;
 
-        this.customerIdValue = id;
+
 
         // Do data fetch
         this.displayCustomerReport();
