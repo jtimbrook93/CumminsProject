10e9e3e0c160e6646295416de5d8df0db7d2cfb6
jtimbrook93 <jtimbroo@iu.edu>
2018-12-11 03:30:34 -0500

    building chart

--- public/JS/sales.app.js
+++ public/JS/sales.app.js
@@ -2,6 +2,18 @@ var salesApp = new Vue({
   el: '#salesMain',
 data: {
 
+    sales: {
+
+          businessSegment: '',
+          percentOfRevenue: '',
+          quarter1Revenue: '',
+          quarter2Revenue: '',
+          quarter3Revenue: '',
+          quarter4Revenue: '',
+          profitMargin: ''
+
+},
+
   DistributionArr: [{
 
     businessSegment: '',
@@ -55,6 +67,7 @@ data: {
         this.fetchFiltration();
         this.fetchDistribution();
         this.fetchPowerGeneration();
+        this.buildSalesChart();
       },
 
       fetchDistribution() {
@@ -120,25 +133,18 @@ data: {
 
           xAxis: {
               categories: [
-                  'Jan',
-                  'Feb',
-                  'Mar',
-                  'Apr',
-                  'May',
-                  'Jun',
-                  'Jul',
-                  'Aug',
-                  'Sep',
-                  'Oct',
-                  'Nov',
-                  'Dec'
+                  'Quarter 1',
+                  'Quarter 2',
+                  'Quarter 3',
+                  'Quarter 4',
+
               ],
               crosshair: true
           },
           yAxis: {
               min: 0,
               title: {
-                  text: 'Revenue in Millions'
+                  text: 'Revenue by Quarter in Millions'
               }
           },
           tooltip: {
@@ -156,20 +162,20 @@ data: {
               }
           },
           series: [{
-              name: 'Tokyo',
-              data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+              name: 'Distribution',
+              data: this.DistributionArr.map( item => [item.quarter1Revenue, item.quarter2Revenue, item.quarter3Revenue, item.quarter4Revenue ])
 
           }, {
-              name: 'New York',
-              data: [83.6, 78.8, 98.5, 93.4, 106.0, 84.5, 105.0, 104.3, 91.2, 83.5, 106.6, 92.3]
+              name: 'Filtration',
+              data: this.FiltrationArr.map( item => [item.quarter1Revenue, item.quarter2Revenue, item.quarter3Revenue, item.quarter4Revenue ])
 
           }, {
-              name: 'London',
-              data: [48.9, 38.8, 39.3, 41.4, 47.0, 48.3, 59.0, 59.6, 52.4, 65.2, 59.3, 51.2]
+              name: 'Engines',
+              data: this.EnginesArr.map( item => [item.quarter1Revenue, item.quarter2Revenue, item.quarter3Revenue, item.quarter4Revenue ])
 
           }, {
-              name: 'Berlin',
-              data: [42.4, 33.2, 34.5, 39.7, 52.6, 75.5, 57.4, 60.4, 47.6, 39.1, 46.8, 51.1]
+              name: 'Power Generation',
+              data: this.PowerGenerationArr.map( item => [item.quarter1Revenue, item.quarter2Revenue, item.quarter3Revenue, item.quarter4Revenue ])
 
           }]
       });
