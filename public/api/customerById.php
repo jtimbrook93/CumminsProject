7b2e7b38a7d68d8d032851295c133fd47c968165
jtimbrook93 <jtimbroo@iu.edu>
2018-12-11 22:04:17 -0500

    asdg

--- public/api/customerById.php
+++ public/api/customerById.php
@@ -7,13 +7,16 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' ) {
   exit;
 }
 
+$Id = intval($_GET['Id'] ?? 0);
 // 1. Go to the database and get all work associated with the $taskId
-$customerArr = Customer::fetchCustomers();
 
+$customerArr2 = Customer::fetchAll($Id);
 
 // 2. Convert to JSON
-$json = json_encode($customerArr,  JSON_PRETTY_PRINT);
+
+$json = json_encode($customerArr2,  JSON_PRETTY_PRINT);
 
 // 3. Print
 header ('Content-type: application/json;charset=utf-8');
-echo json_encode($customerArr);
+
+echo json_encode($customerArr2);
